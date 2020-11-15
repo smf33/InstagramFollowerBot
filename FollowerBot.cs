c5561756df0743b83e9755491eddbb2a510aac86
SmF33 <53909358+smf33@users.noreply.github.com>
2020-11-15 12:42:07 +0100

    Init Azure AI

--- FollowerBot.cs
+++ FollowerBot.cs
@@ -4,214 +4,290 @@ using System.IO;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.DependencyCollector;
 using Microsoft.Extensions.Logging;
 
 namespace InstagramFollowerBot
 {
-	public partial class FollowerBot : IDisposable
-	{
-		private const string DetectContactsFollowBackStr = "DETECTCONTACTSFOLLOWBACK";
-		private const string DetectContactsUnfollowBackStr = "DETECTCONTACTSUNFOLLOWBACK";
-		private const string DoContactsFollowStr = "DOCONTACTSFOLLOW";
-		private const string DoContactsUnfollowStr = "DOCONTACTSUNFOLLOW";
-		private const string DoPhotosLikeStr = "DOPHOTOSLIKE";
-		private const string DoPhotosLikeJustFollowStr = "DOPHOTOSLIKE_FOLLOWONLY";
-		private const string DoPhotosLikeJustLikeStr = "DOPHOTOSLIKE_LIKEONLY";
-		private const string ExplorePhotosStr = "EXPLOREPHOTOS";
-		private const string ExplorePeopleSuggestedStr = "EXPLOREPEOPLESUGGESTED";
-		private const string LoopStartStr = "LOOPSTART";
-		private const string LoopStr = "LOOP";
-		private const string PauseStr = "PAUSE";
-		private const string SearchKeywordsStr = "SEARCHKEYWORDS";
-		private const string SaveStr = "SAVE";
-		private const string WaitStr = "WAIT";
+    public partial class FollowerBot : IDisposable
+    {
+        private const string DetectContactsFollowBackStr = "DETECTCONTACTSFOLLOWBACK";
+        private const string DetectContactsUnfollowBackStr = "DETECTCONTACTSUNFOLLOWBACK";
+        private const string DoContactsFollowStr = "DOCONTACTSFOLLOW";
+        private const string DoContactsUnfollowStr = "DOCONTACTSUNFOLLOW";
+        private const string DoPhotosLikeStr = "DOPHOTOSLIKE";
+        private const string DoPhotosLikeJustFollowStr = "DOPHOTOSLIKE_FOLLOWONLY";
+        private const string DoPhotosLikeJustLikeStr = "DOPHOTOSLIKE_LIKEONLY";
+        private const string ExplorePhotosStr = "EXPLOREPHOTOS";
+        private const string ExplorePeopleSuggestedStr = "EXPLOREPEOPLESUGGESTED";
+        private const string LoopStartStr = "LOOPSTART";
+        private const string LoopStr = "LOOP";
+        private const string PauseStr = "PAUSE";
+        private const string SearchKeywordsStr = "SEARCHKEYWORDS";
+        private const string SaveStr = "SAVE";
+        private const string WaitStr = "WAIT";
 
-		private static readonly string ExecPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private static readonly string ExecPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-		private readonly ILogger Log;
+        private readonly ILogger Log;
+        private readonly TelemetryClient telemetryClient;
+        private readonly DependencyTrackingTelemetryModule aiDependencyTrackingTelemetryModule; // even if not used, must be kept during all the app life
 
-		public FollowerBot(string[] configArgs, ILogger logger)
-		{
-			Log = logger;
+        public FollowerBot(string[] configArgs, ILogger logger, TelemetryClient telemetryCli)
+        {
+            Log = logger;
+            telemetryClient = telemetryCli;
 
-			Log.LogInformation("## LOADING...");
-			LoadConfig(configArgs);
+            Log.LogInformation("## LOADING...");
+            var opLoading = telemetryClient.StartOperation(new RequestTelemetry { Name = "LOADING" });
+            try
+            {
+                LoadConfig(configArgs);
+                telemetryClient.Context.User.Id = Config.BotUserEmail;
 
-			LoadData();
+                LoadData();
 
-			string w = Rand.Next(Config.SeleniumWindowMinW, Config.SeleniumWindowMaxW).ToString(CultureInfo.InvariantCulture);
-			string h = Rand.Next(Config.SeleniumWindowMinH, Config.SeleniumWindowMaxH).ToString(CultureInfo.InvariantCulture);
-			if (string.IsNullOrWhiteSpace(Config.SeleniumRemoteServer))
-			{
-				Log.LogDebug("NewChromeSeleniumWrapper({0}, {1}, {2})", ExecPath, w, h);
-				Selenium = SeleniumWrapper.NewChromeSeleniumWrapper(ExecPath, w, h, Config.SeleniumBrowserArguments, Config.BotSeleniumTimeoutSec);
-			}
-			else
-			{
-				if (Config.SeleniumRemoteServerWarmUpWaitMs > 0)
-				{
-					Task.Delay(Config.SeleniumRemoteServerWarmUpWaitMs)
-						.Wait();
-				}
-				Log.LogDebug("NewRemoteSeleniumWrapper({0}, {1}, {2})", Config.SeleniumRemoteServer, w, h);
-				Selenium = SeleniumWrapper.NewRemoteSeleniumWrapper(Config.SeleniumRemoteServer, w, h, Config.SeleniumBrowserArguments, Config.BotSeleniumTimeoutSec);
-			}
-		}
+                string w = Rand.Next(Config.SeleniumWindowMinW, Config.SeleniumWindowMaxW).ToString(CultureInfo.InvariantCulture);
+                string h = Rand.Next(Config.SeleniumWindowMinH, Config.SeleniumWindowMaxH).ToString(CultureInfo.InvariantCulture);
+                if (string.IsNullOrWhiteSpace(Config.SeleniumRemoteServer))
+                {
+                    Log.LogDebug("NewChromeSeleniumWrapper({0}, {1}, {2})", ExecPath, w, h);
+                    Selenium = SeleniumWrapper.NewChromeSeleniumWrapper(ExecPath, w, h, Config.SeleniumBrowserArguments, Config.BotSeleniumTimeoutSec);
+                }
+                else
+                {
+                    if (Config.SeleniumRemoteServerWarmUpWaitMs > 0)
+                    {
+                        Task.Delay(Config.SeleniumRemoteServerWarmUpWaitMs)
+                            .Wait();
+                    }
+                    Log.LogDebug("NewRemoteSeleniumWrapper({0}, {1}, {2})", Config.SeleniumRemoteServer, w, h);
+                    Selenium = SeleniumWrapper.NewRemoteSeleniumWrapper(Config.SeleniumRemoteServer, w, h, Config.SeleniumBrowserArguments, Config.BotSeleniumTimeoutSec);
+                }
+                opLoading.Telemetry.Success = true;
+            }
+            catch (Exception e)
+            {
+                telemetryClient.TrackException(e);
+                opLoading.Telemetry.Success = false;
+                throw new ApplicationException("LOADING Exception", e);
+            }
+            finally
+            {
+                telemetryClient.StopOperation(opLoading);
+            }
+        }
 
-		public void Run()
-		{
-			Log.LogInformation("## LOGGING...");
+        public void Run()
+        {
+            Log.LogInformation("## LOGGING...");
+            var opLogging = telemetryClient.StartOperation(new RequestTelemetry { Name = "LOGGING", Url = new Uri(string.Concat(Config.UrlRoot, Config.UrlLogin, "?logging=", Config.BotUserEmail)) });
+            try
+            {
+                if (Data.UserContactUrl == null || !TryAuthCookies())
+                {
+                    AuthLogin();
+                }
+                Log.LogInformation("Logged user :  {0}", Data.UserContactUrl);
+                PostAuthInit();
+                SaveData(); // save cookies at last
 
-			if (Data.UserContactUrl == null
-				|| !TryAuthCookies())
-			{
-				AuthLogin();
-			}
-			Log.LogInformation("Logged user :  {0}", Data.UserContactUrl);
-			PostAuthInit();
-			SaveData(); // save cookies at last
+                opLogging.Telemetry.Success = true;
+            }
+            catch (Exception e)
+            {
+                telemetryClient.TrackException(e);
+                opLogging.Telemetry.Success = false;
+                throw new ApplicationException("LOGGING Exception", e);
+            }
+            finally
+            {
+                telemetryClient.StopOperation(opLogging);
+            }
 
-			Log.LogInformation("## RUNNING...");
-			string[] tasks = Config.BotTasks.Split(',', StringSplitOptions.RemoveEmptyEntries);
-			for (int i = 0; i < tasks.Length; i++)
-			{
-				tasks[i] = tasks[i].Trim().ToUpperInvariant(); // standardize
-			}
-			int iLoop = Config.BotLoopTaskLimited;
-			for (int i = 0; i < tasks.Length; i++)
-			{
-				string curTask = tasks[i];
-				Log.LogInformation("# {0}...", curTask);
-				switch (curTask)
-				{
-					case DetectContactsFollowBackStr:
-						DetectContactsFollowBack();
-						break;
-					case DetectContactsUnfollowBackStr:
-						DetectContactsUnfollowBack();
-						break;
-					case DoContactsFollowStr:
-						DoContactsFollow();
-						break;
-					case DoContactsUnfollowStr:
-						DoContactsUnfollow();
-						break;
-					case DoPhotosLikeStr:
-						DoPhotosLike();
-						break;
-					case DoPhotosLikeJustFollowStr:
-						DoPhotosLike(true, false);
-						break;
-					case DoPhotosLikeJustLikeStr:
-						DoPhotosLike(false, true);
-						break;
-					case ExplorePhotosStr:
-						ExplorePhotos();
-						break;
-					case ExplorePeopleSuggestedStr:
-						ExplorePeopleSuggested();
-						break;
-					case SaveStr: // managed in the if after
-						break;
-					case SearchKeywordsStr:
-						SearchKeywords();
-						break;
-					case PauseStr:
-					case WaitStr:
-						Task.Delay(Rand.Next(Config.BotWaitTaskMinWaitSec, Config.BotWaitTaskMaxWaitSec))
-							.Wait();
-						continue; // no save anyway
-					case LoopStartStr:
-						continue; // no save anyway
-					case LoopStr:
-						if (Config.BotLoopTaskLimited <= 0)
-						{
-							i = Array.IndexOf(tasks, LoopStartStr); // -1 (so ok) if not found
-						}
-						else if (iLoop > 0)
-						{
-							Log.LogDebug("Loop still todo : {0}", iLoop);
-							iLoop--;
-							i = Array.IndexOf(tasks, LoopStartStr); // -1 (so ok) if not found
-						}
-						if (Config.BotSaveOnLoop)
-						{
-							curTask = SaveStr;
-						}
-						break;
-					default:
-						Log.LogError("Unknown BotTask : {0}", tasks[i]);
-						break;
-				}
-				if (Config.BotSaveAfterEachAction || curTask == SaveStr)
-				{
-					SaveData();
-				}
-			}
-			if (Config.BotSaveOnEnd)
-			{
-				SaveData();
-			}
-			Log.LogInformation("## ENDED OK");
-		}
+            Log.LogInformation("## RUNNING...");
+            var opRunning = telemetryClient.StartOperation(new RequestTelemetry { Name = string.Concat("RUNNING ", Config.BotTasks), Url = new Uri(string.Concat(Data.UserContactUrl, "?running=", Config.BotTasks)) });
+            try
+            {
 
-		internal void DebugDump()
-		{
-			StringBuilder dump = new StringBuilder();
-			try
-			{
-				dump.AppendFormat("# Try Dump last page : {0} @ {1}\r\n", Selenium.Title, Selenium.Url);
-				dump.Append(Selenium.CurrentPageSource); // this one may crash more probably
-			}
-			catch
-			{
-				// Not usefull because already in exception
-			}
+                string[] tasks = Config.BotTasks.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < tasks.Length; i++)
+                {
+                    tasks[i] = tasks[i].Trim().ToUpperInvariant(); // standardize
+                }
+                int iLoop = Config.BotLoopTaskLimited;
+                for (int i = 0; i < tasks.Length; i++)
+                {
+                    string curTask = tasks[i];
+                    Log.LogInformation("# {0}...", curTask);
+                    var opTask = telemetryClient.StartOperation(new RequestTelemetry { Name = string.Concat("TASK ", curTask), Url = new Uri(string.Concat(Data.UserContactUrl, "?task=", curTask)) });
+                    try
+                    {
+                        switch (curTask)
+                        {
+                            case DetectContactsFollowBackStr:
+                                DetectContactsFollowBack();
+                                break;
+                            case DetectContactsUnfollowBackStr:
+                                DetectContactsUnfollowBack();
+                                break;
+                            case DoContactsFollowStr:
+                                DoContactsFollow();
+                                break;
+                            case DoContactsUnfollowStr:
+                                DoContactsUnfollow();
+                                break;
+                            case DoPhotosLikeStr:
+                                DoPhotosLike();
+                                break;
+                            case DoPhotosLikeJustFollowStr:
+                                DoPhotosLike(true, false);
+                                break;
+                            case DoPhotosLikeJustLikeStr:
+                                DoPhotosLike(false, true);
+                                break;
+                            case ExplorePhotosStr:
+                                ExplorePhotos();
+                                break;
+                            case ExplorePeopleSuggestedStr:
+                                ExplorePeopleSuggested();
+                                break;
+                            case SaveStr: // managed in the if after
+                                break;
+                            case SearchKeywordsStr:
+                                SearchKeywords();
+                                break;
+                            case PauseStr:
+                            case WaitStr:
+                                Task.Delay(Rand.Next(Config.BotWaitTaskMinWaitSec, Config.BotWaitTaskMaxWaitSec))
+                                    .Wait();
+                                continue; // no save anyway
+                            case LoopStartStr:
+                                continue; // no save anyway
+                            case LoopStr:
+                                if (Config.BotLoopTaskLimited <= 0)
+                                {
+                                    i = Array.IndexOf(tasks, LoopStartStr); // -1 (so ok) if not found
+                                }
+                                else if (iLoop > 0)
+                                {
+                                    Log.LogDebug("Loop still todo : {0}", iLoop);
+                                    iLoop--;
+                                    i = Array.IndexOf(tasks, LoopStartStr); // -1 (so ok) if not found
+                                }
+                                if (Config.BotSaveOnLoop)
+                                {
+                                    curTask = SaveStr;
+                                }
+                                break;
+                            default:
+                                Log.LogError("Unknown BotTask : {0}", tasks[i]);
+                                break;
+                        }
 
-			if (dump.Length > 0)
-			{
-				Log.LogDebug(dump.ToString());
-			}
-			else
-			{
-				Log.LogDebug("# Couldn't dump last page context");
-			}
+                        opTask.Telemetry.Success = true;
+                    }
+                    catch (Exception e)
+                    {
+                        telemetryClient.TrackException(e);
+                        opTask.Telemetry.Success = false;
+                        throw new ApplicationException(string.Concat(curTask, " Exception"), e);
+                    }
+                    finally
+                    {
+                        telemetryClient.StopOperation(opTask);
+                    }
 
-			try
-			{
-				Log.LogDebug("# Try saving Data in order to avoid queue polution");
-				SaveData();
-			}
-			catch
-			{
-				// Not usefull because already in exception
-			}
-			
-		}
+                    if (Config.BotSaveAfterEachAction || curTask == SaveStr)
+                    {
+                        SaveData();
+                    }
+                }
+                if (Config.BotSaveOnEnd)
+                {
+                    SaveData();
+                }
 
-		#region IDisposable Support
-		private bool disposedValue = false; // To detect redundant calls
-		private SeleniumWrapper Selenium;
+                opRunning.Telemetry.Success = true;
+            }
+            catch (ApplicationException)
+            {
+                opRunning.Telemetry.Success = false;
+                throw;
+            }
+            catch (Exception e)
+            {
+                telemetryClient.TrackException(e);
+                opRunning.Telemetry.Success = false;
+                throw new ApplicationException("RUNNING Exception", e);
+            }
+            finally
+            {
+                telemetryClient.StopOperation(opRunning);
+            }
 
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!disposedValue)
-			{
-				if (disposing)
-				{
-					Selenium.Dispose();
-					Selenium = null;
-				}
-				disposedValue = true;
-			}
-		}
+            Log.LogInformation("## ENDED OK");
+        }
 
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
-		#endregion
+        internal void DebugDump()
+        {
+            StringBuilder dump = new StringBuilder();
+            try
+            {
+                dump.AppendFormat("# Try Dump last page : {0} @ {1}\r\n", Selenium.Title, Selenium.Url);
+                dump.Append(Selenium.CurrentPageSource); // this one may crash more probably
+            }
+            catch
+            {
+                // Not usefull because already in exception
+            }
 
-	}
+            if (dump.Length > 0)
+            {
+                Log.LogDebug(dump.ToString());
+            }
+            else
+            {
+                Log.LogDebug("# Couldn't dump last page context");
+            }
+
+            try
+            {
+                Log.LogDebug("# Try saving Data in order to avoid queue polution");
+                SaveData();
+            }
+            catch
+            {
+                // Not usefull because already in exception
+            }
+
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+        private SeleniumWrapper Selenium;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Selenium.Dispose();
+                    Selenium = null;
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,36 +1,61 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Extensions.Logging;
 
 namespace InstagramFollowerBot
 {
-	public class Program
-	{
+    public class Program
+    {
 
-		private static int Main(string[] args)
-		{
-			ConsoleLogger logger = new ConsoleLogger();
+        private static int Main(string[] args)
+        {
+            int ret;
+            ConsoleLogger logger = new ConsoleLogger();
 
-			try
-			{
-				using FollowerBot bot = new FollowerBot(args, logger);
-				try
-				{
-					bot.Run();
-				}
-				catch
-				{
-					bot.DebugDump();
-					throw;
-				}
-			}
-			catch (Exception ex)
-			{
-				logger.LogCritical(default, ex, "## ENDED IN ERROR : {0}", ex.GetBaseException().Message);
-				return -1;
-			}
+            TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
+            TelemetryClient telemetryClient = new TelemetryClient(configuration);
 
-			return 0;
-		}
+            try
+            {
+                using DependencyTrackingTelemetryModule aiDependencyTrackingTelemetryModule = new DependencyTrackingTelemetryModule();
+                aiDependencyTrackingTelemetryModule.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.windows.net");
+                aiDependencyTrackingTelemetryModule.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("localhost");
+                aiDependencyTrackingTelemetryModule.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("127.0.0.1");
+                aiDependencyTrackingTelemetryModule.Initialize(configuration);
+                configuration.TelemetryInitializers.Add(new HttpDependenciesParsingTelemetryInitializer());
 
-	}
+                using FollowerBot bot = new FollowerBot(args, logger, telemetryClient);
+                try
+                {
+                    bot.Run();
+                }
+                catch
+                {
+                    bot.DebugDump();
+                    throw;
+                }
+                ret = 0;
+            }
+            catch (ApplicationException ex)
+            {
+                logger.LogCritical(default, ex, "## ENDED IN ERROR : {0}", ex.GetBaseException().Message);
+                ret = -1;
+            }
+            catch (Exception ex)
+            {
+                telemetryClient.TrackException(ex);
+                logger.LogCritical(default, ex, "## ENDED IN ERROR : {0}", ex.GetBaseException().Message);
+                ret = -2;
+            }
+
+            telemetryClient.Flush();
+            Task.Delay(5000).Wait();
+
+            return ret;
+        }
+
+    }
 }
